6fec1cfb2db48e973ce984a21384fa041766de7a
ardi <ardifjar443@gmail.com>
2024-01-26 22:15:30 +0700

    update : latihan streamlite

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,46 +1,27 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
-import av
-import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
-import cv2
-from PIL import Image
+import numpy as np
 
 # Load the saved model
 model = load_model('model/best_model.h5')
 
 st.title('Emotion Detection App')
 
-# Helper function to preprocess image
-def preprocess_image(img):
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+if uploaded_file is not None:
+    # Preprocess the image for the model
+    img = image.load_img(uploaded_file, target_size=(48, 48))
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array /= 255.0  # Normalize the image
-    return img_array
 
-# Function to predict emotion from image
-def predict_emotion(img_array):
+    # Make predictions
     predictions = model.predict(img_array)
+
+    # Get the predicted emotion
     emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
     predicted_emotion = emotions[np.argmax(predictions)]
-    return predicted_emotion
 
-class VideoTransformer(VideoTransformerBase):
-    def transform(self, frame):
-        # Convert the BGR image to RGB
-        img_rgb = cv2.cvtColor(frame.to_ndarray(format="bgr24"), cv2.COLOR_BGR2RGB)
-
-        # Convert to PIL Image
-        img_pil = Image.fromarray(img_rgb)
-
-        # Resize image to match the model input size
-        img_resized = img_pil.resize((48, 48))
-
-        # Preprocess the image
-        img_array = preprocess_image(img_resized)
-
-        # Make prediction
-        predicted_emotion = predict_emotion(img_array)
-
-        # Display the image and predict
+    st.image(img, caption=f'Predicted Emotion: {predicted_emotion}', use_column_width=True)
