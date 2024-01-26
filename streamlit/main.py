b403997692d6df1cc5d41b679eca5c29c9a6c3a7
ardi <ardifjar443@gmail.com>
2024-01-26 22:26:29 +0700

    update : latihan streamlite

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,27 +1,33 @@
 import streamlit as st
-from tensorflow.keras.models import load_model
+from tensorflow import keras
 from tensorflow.keras.preprocessing import image
 import numpy as np
 
 # Load the saved model
-model = load_model('model/best_model.h5')
+model = keras.models.load_model('model/best_model.h5')
 
 st.title('Emotion Detection App')
 
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Preprocess the image for the model
-    img = image.load_img(uploaded_file, target_size=(48, 48))
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.0  # Normalize the image
+    try:
+        # Preprocess the image for the model
+        img = image.load_img(uploaded_file, target_size=(48, 48))
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array /= 255.0  # Normalize the image
 
-    # Make predictions
-    predictions = model.predict(img_array)
+        # Make predictions
+        predictions = model.predict(img_array)
 
-    # Get the predicted emotion
-    emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
-    predicted_emotion = emotions[np.argmax(predictions)]
+        # Get the predicted emotion
+        emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+        predicted_emotion = emotions[np.argmax(predictions)]
 
-    st.image(img, caption=f'Predicted Emotion: {predicted_emotion}', use_column_width=True)
+        # Display the original image and predicted emotion
+        st.image(img, caption=f'Predicted Emotion: {predicted_emotion}', use_column_width=True)
+    except Exception as e:
+        st.error(f"Error: {e}")
+else:
+    st.info("Please upload an image.")
